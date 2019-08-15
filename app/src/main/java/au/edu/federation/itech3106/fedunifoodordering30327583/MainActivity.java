e2c8f2dd3d102cd9af6e441686b9c9957e92553f
wangzhest <wangzhest@outlook.com>
2019-08-16 00:30:09 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/federation/itech3106/fedunifoodordering30327583/MainActivity.java
@@ -0,0 +1,19 @@
+package au.edu.federation.itech3106.fedunifoodordering30327583;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.iv_show);
+    }
+}
---
+++ app/src/main/java/au/edu/federation/itech3106/fedunifoodordering30327583/OrderBurgerActivity.java
@@ -0,0 +1,17 @@
+package au.edu.federation.itech3106.fedunifoodordering30327583;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderBurgerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_burger);
+        initView();
+    }
+
+    private void initView() {
+    }
+}
